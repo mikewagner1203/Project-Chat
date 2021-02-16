4f64bdbf117a258ecbc4bc64af54bd276f006270
Michael Wagner <Mikewagner@gmx.at>
2021-02-16 14:08:57 +0100

    added funcomment :)

--- src/ClientConnection.java
+++ src/ClientConnection.java
@@ -56,6 +56,8 @@ public class ClientConnection implements Runnable {
         }
     }
 
+    /// irgendein kommentar
+
     //sends message back to client
     public void sendMessage(String message) {
         try {
